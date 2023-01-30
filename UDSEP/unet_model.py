7fd6be4e388703a87edd22d9f8f4c02afe287c49
joaomouraaaa <80763144+joaomouraaaa@users.noreply.github.com>
2023-01-30 10:08:41 -0100

    Add files via upload

---
+++ UDSEP/unet_model.py
@@ -0,0 +1,145 @@
+# u-net model with up-convolution or up-sampling and weighted binary-crossentropy as loss func
+
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout
+# from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
+from keras.utils.vis_utils import plot_model
+# from keras.utils import plot_model
+from keras import backend as K
+import numpy as np
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+
+
+def identify_axis(shape):
+    # Three dimensional
+    if len(shape) == 5 : return [1,2,3]
+    # Two dimensional
+    elif len(shape) == 4 : return [1,2]
+    # Exception - Unknown
+    else : raise ValueError('Metric: Shape of tensor is neither 2D or 3D.')
+    
+def unet_model(n_classes, class_weights, im_sz=256, n_channels=1, n_filters_start=32, growth_factor=2, upconv=True): 
+#def unet_model(n_classes=2, im_sz=256, n_channels=3, n_filters_start=32, growth_factor=2, upconv=True,class_weights=[1, 1 , 1]):
+    droprate=0.25
+    n_filters = n_filters_start
+    inputs = Input((im_sz, im_sz, n_channels))
+    #inputs = BatchNormalization()(inputs)
+    conv1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    #pool1 = Dropout(droprate)(pool1)
+
+    n_filters *= growth_factor
+    pool1 = BatchNormalization()(pool1)
+    conv2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    pool2 = Dropout(droprate)(pool2)
+
+    n_filters *= growth_factor
+    pool2 = BatchNormalization()(pool2)
+    conv3 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    pool3 = Dropout(droprate)(pool3)
+
+    n_filters *= growth_factor
+    pool3 = BatchNormalization()(pool3)
+    conv4_0 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool3)
+    conv4_0 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv4_0)
+    pool4_1 = MaxPooling2D(pool_size=(2, 2))(conv4_0)
+    pool4_1 = Dropout(droprate)(pool4_1)
+
+    n_filters *= growth_factor
+    pool4_1 = BatchNormalization()(pool4_1)
+    conv4_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool4_1)
+    conv4_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv4_1)
+    pool4_2 = MaxPooling2D(pool_size=(2, 2))(conv4_1)
+    pool4_2 = Dropout(droprate)(pool4_2)
+
+    n_filters *= growth_factor
+    conv5 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool4_2)
+    conv5 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv5)
+
+    n_filters //= growth_factor
+    if upconv:
+        up6_1 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv5), conv4_1])
+    else:
+        up6_1 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4_1])
+    up6_1 = BatchNormalization()(up6_1)
+    conv6_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(up6_1)
+    conv6_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv6_1)
+    conv6_1 = Dropout(droprate)(conv6_1)
+
+    n_filters //= growth_factor
+    if upconv:
+        up6_2 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv6_1), conv4_0])
+    else:
+        up6_2 = concatenate([UpSampling2D(size=(2, 2))(conv6_1), conv4_0])
+    up6_2 = BatchNormalization()(up6_2)
+    conv6_2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(up6_2)
+    conv6_2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv6_2)
+    conv6_2 = Dropout(droprate)(conv6_2)
+
+    n_filters //= growth_factor
+    if upconv:
+        up7 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv6_2), conv3])
+    else:
+        up7 = concatenate([UpSampling2D(size=(2, 2))(conv6_2), conv3])
+    up7 = BatchNormalization()(up7)
+    conv7 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv7)
+    conv7 = Dropout(droprate)(conv7)
+
+    n_filters //= growth_factor
+    if upconv:
+        up8 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv7), conv2])
+    else:
+        up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2])
+    up8 = BatchNormalization()(up8)
+    conv8 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv8)
+    conv8 = Dropout(droprate)(conv8)
+
+    n_filters //= growth_factor
+    if upconv:
+        up9 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv8), conv1])
+    else:
+        up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1])
+    conv9 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(n_classes, (1, 1), activation='softmax')(conv9)
+
+    model = Model(inputs=inputs, outputs=conv10)
+
+    
+    def weighted_crossentropy(y_true, y_pred):
+        class_loglosses = K.mean(K.binary_crossentropy(y_true, y_pred), axis=[0, 1])
+        return K.sum(class_loglosses * K.constant(class_weights))
+
+
+    def focal_loss(y_true, y_pred):
+        alpha=0.25
+        gamma_f=2.0
+        
+        axis = identify_axis(y_true.get_shape())
+        # Clip values to prevent division by zero error
+        epsilon = K.epsilon()
+        y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+        cross_entropy = -y_true * K.log(y_pred)
+        
+
+        if alpha is not None:
+            alpha_weight = np.array(alpha, dtype=np.float32)
+            focal_loss = alpha_weight * K.pow(1 - y_pred, gamma_f) * cross_entropy
+      
+
+        focal_loss = K.mean(K.sum(focal_loss, axis=[-1]))
+        return focal_loss
+
+    #model.compile(optimizer=Adam(), loss=focal_loss,metrics = ['accuracy'])
+    model.compile(optimizer=Adam(), loss=weighted_crossentropy,metrics = ['accuracy'])
+
+    return model
